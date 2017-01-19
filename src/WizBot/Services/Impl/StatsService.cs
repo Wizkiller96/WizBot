c3083fb4df0ec1a5437573d02afbcc21fa66e7ee
Wizkiller96 <wizkiller96@hotmail.com>
2017-01-19 10:11:22 -0500

    Version upped to 1.1.3

--- src/WizBot/Services/Impl/StatsService.cs
+++ src/WizBot/Services/Impl/StatsService.cs
@@ -15,7 +15,7 @@ namespace WizBot.Services.Impl
         private DiscordShardedClient client;
         private DateTime started;
 
-        public const string BotVersion = "1.1.2";
+        public const string BotVersion = "1.1.3";
 
         public string Author => "Kwoth#2560 & Wizkiller96#2947";
         public string Library => "Discord.Net";
