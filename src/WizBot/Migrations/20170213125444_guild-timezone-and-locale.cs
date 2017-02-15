3080296da9f496feec5dca6d3e1a1012c7dc7b64
Wizkiller96 <wizkiller96@hotmail.com>
2017-02-15 14:06:45 -0500

    fixed migration

--- src/WizBot/Migrations/20170213125444_guild-timezone-and-locale.cs
+++ src/WizBot/Migrations/20170213125444_guild-timezone-and-locale.cs
@@ -17,6 +17,7 @@ namespace WizBot.Migrations
             migrationBuilder.AddColumn<string>(
                 name: "TimeZoneId",
                 table: "GuildConfigs",
+                nullable: true,
                 defaultValue: null);
         }
 
