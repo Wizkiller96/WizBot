70e0b48ae0d4b88dd2de08340127d9671d1204b1
Wizkiller96 <wizkiller96@hotmail.com>
2017-02-15 22:12:30 -0500

    1.1.8-alpha

--- src/WizBot/Services/Impl/StatsService.cs
+++ src/WizBot/Services/Impl/StatsService.cs
@@ -15,7 +15,7 @@ namespace WizBot.Services.Impl
         private DiscordShardedClient client;
         private DateTime started;
 
-        public const string BotVersion = "1.1.6";
+        public const string BotVersion = "1.1.8-alpha";
 
         public string Author => "Kwoth#2560 & Wizkiller96#2947";
         public string Library => "Discord.Net";
