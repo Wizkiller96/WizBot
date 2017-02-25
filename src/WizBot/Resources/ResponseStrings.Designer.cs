fabcf28cfa9bb1fdbfc9383999354151d15b4061
Wizkiller96 <wizkiller96@hotmail.com>
2017-02-24 22:00:04 -0500

    fairplay key was missing

--- src/WizBot/Resources/ResponseStrings.Designer.cs
+++ src/WizBot/Resources/ResponseStrings.Designer.cs
@@ -3443,6 +3443,15 @@ namespace WizBot.Resources {
         }
         
         /// <summary>
+        ///    Looks up a localized string similar to fairplay.
+        /// </summary>
+        public static string music_fairplay {
+            get {
+                return ResourceManager.GetString("music_fairplay", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///    Looks up a localized string similar to Finished Song.
         /// </summary>
         public static string music_finished_song {
