75871f8450be86c0072b22946e57a5773fbe2f0c
Wizkiller96 <wizkiller96@hotmail.com>
2017-04-02 09:34:08 -0400

    .slowmodewl implemented and now works properly

--- src/WizBot/Services/Database/Repositories/Impl/GuildConfigRepository.cs
+++ src/WizBot/Services/Database/Repositories/Impl/GuildConfigRepository.cs
@@ -38,6 +38,8 @@ namespace WizBot.Services.Database.Repositories.Impl
                 .Include(gc => gc.CommandCooldowns)
                 .Include(gc => gc.GuildRepeaters)
                 .Include(gc => gc.AntiRaidSetting)
+                .Include(gc => gc.SlowmodeIgnoredRoles)
+                .Include(gc => gc.SlowmodeIgnoredUsers)
                 .Include(gc => gc.AntiSpamSetting)
                     .ThenInclude(x => x.IgnoredChannels)
                 .ToList();
