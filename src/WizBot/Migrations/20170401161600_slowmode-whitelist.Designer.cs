f4d24b5445a745aa9517daca751c52fe4260c1fc
Wizkiller96 <wizkiller96@hotmail.com>
2017-04-02 09:04:45 -0400

    Slowmode whitelists done, need testing

--- src/WizBot/Migrations/20170401161600_slowmode-whitelist.Designer.cs
+++ src/WizBot/Migrations/20170401161600_slowmode-whitelist.Designer.cs
@@ -10,9 +10,10 @@ using WizBot.Modules.Music.Classes;
 namespace WizBot.Migrations
 {
     [DbContext(typeof(WizBotContext))]
-    partial class WizBotSqliteContextModelSnapshot : ModelSnapshot
+    [Migration("20170401161600_slowmode-whitelist")]
+    partial class slowmodewhitelist
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
