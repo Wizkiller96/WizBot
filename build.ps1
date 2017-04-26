d44cc52e9312d3c06fefc2ba1c5b8ed5e6727553
Wizkiller96 <wizkiller96@hotmail.com>
2017-04-26 00:11:01 -0400

    First try in publishing nadeko to myget

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-appveyor-retry dotnet restore NadekoBot.sln -v Minimal /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
+appveyor-retry dotnet restore WizBot.sln -v Minimal /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
-dotnet build NadekoBot.sln -c "Release" /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
+dotnet build WizBot.sln -c "Release" /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
