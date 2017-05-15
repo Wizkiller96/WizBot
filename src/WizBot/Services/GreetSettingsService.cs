12ede643f275381420d889e8bcc1c0eb63da8adb
Wizkiller96 <wizkiller96@hotmail.com>
2017-05-15 17:38:16 -0400

    Forgot to cache if SetSettings is used

--- src/WizBot/Services/GreetSettingsService.cs
+++ src/WizBot/Services/GreetSettingsService.cs
@@ -67,6 +67,9 @@ namespace WizBot.Services
                 conf.SendChannelByeMessage = settings.SendChannelByeMessage;
 
                 await uow.CompleteAsync().ConfigureAwait(false);
+
+                var toAdd = GreetSettings.Create(conf);
+                GuildConfigsCache.AddOrUpdate(guildId, toAdd, (key, old) => toAdd);
             }
 
             return true;
