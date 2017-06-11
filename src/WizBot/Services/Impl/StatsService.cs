cacb47508f7ff1ea9550e77fb12590aa36e1f255
Wizkiller96 <wizkiller96@hotmail.com>
2017-06-10 22:17:34 -0400

    Republish, version is 1.41.1 now

--- src/WizBot/Services/Impl/StatsService.cs
+++ src/WizBot/Services/Impl/StatsService.cs
@@ -17,7 +17,7 @@ namespace WizBot.Services.Impl
         private readonly IBotCredentials _creds;
         private readonly DateTime _started;
 
-        public const string BotVersion = "1.4.1-beta2";
+        public const string BotVersion = "1.41.1";
 
         public string Author => "Kwoth#2560 & Wizkiller96#2947";
         public string Library => "Discord.Net";
