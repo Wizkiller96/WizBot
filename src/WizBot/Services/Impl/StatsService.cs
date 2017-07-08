08f51752ccee1bb4bfbf48e2e36dd5ea6ea47516
Wizkiller96 <wizkiller96@hotmail.com>
2017-07-08 01:42:18 -0400

    1.54b

--- src/WizBot/Services/Impl/StatsService.cs
+++ src/WizBot/Services/Impl/StatsService.cs
@@ -17,7 +17,7 @@ namespace WizBot.Services.Impl
         private readonly IBotCredentials _creds;
         private readonly DateTime _started;
 
-        public const string BotVersion = "1.54a";
+        public const string BotVersion = "1.54b";
 
         public string Author => "Kwoth#2560 & Wizkiller96#2947";
         public string Library => "Discord.Net";
