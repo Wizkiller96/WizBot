f4f53126575a6ac0484d725057ca7a5426ace6ad
Wizkiller96 <wizkiller96@hotmail.com>
2017-08-15 23:49:44 -0400

    Antispam can now take an extra parameter at the end which is the time of the mute. It will be ignored if punishment type isn't mute

---
+++ src/WizBot/Migrations/20170815222316_mute-time-antispam.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizBot.Migrations
+{
+    public partial class mutetimeantispam : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MuteTime",
+                table: "AntiSpamSetting",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MuteTime",
+                table: "AntiSpamSetting");
+        }
+    }
+}
--- src/WizBot/Services/Database/Models/AntiProtection.cs
+++ src/WizBot/Services/Database/Models/AntiProtection.cs
@@ -18,6 +18,7 @@ namespace WizBot.Services.Database.Models
 
         public PunishmentAction Action { get; set; }
         public int MessageThreshold { get; set; } = 3;
+        public int MuteTime { get; set; } = 0;
         public HashSet<AntiSpamIgnore> IgnoredChannels { get; set; } = new HashSet<AntiSpamIgnore>();
     }
 
