669a80c9d81082c753353d67afaad4cc32333d7e
Wizkiller96 <wizkiller96@hotmail.com>
2017-09-13 11:48:22 -0400

    Fixed slow `.xp` and `.xpglb`

---
+++ src/WizBot/Migrations/20170913022654_total-xp.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizBot.Migrations
+{
+    public partial class totalxp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalXp",
+                table: "DiscordUser",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.Sql(MigrationQueries.TotalXp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalXp",
+                table: "DiscordUser");
+        }
+    }
+}
