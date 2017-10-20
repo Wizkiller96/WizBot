518512aa4858f872cc772e0674168dc91e682083
Wizkiller96 <wizkiller96@hotmail.com>
2017-10-20 16:13:34 -0400

    Currency Raffle almost done.

--- WizBot.Core/Modules/Gambling/CurrencyRaffleCommands.cs
+++ WizBot.Core/Modules/Gambling/CurrencyRaffleCommands.cs
@@ -1,6 +1,10 @@
 using WizBot.Common.Attributes;
 using WizBot.Core.Modules.Gambling.Services;
 using System.Threading.Tasks;
+using Discord;
+using System;
+using WizBot.Core.Services;
+using WizBot.Extensions;
 
 namespace WizBot.Modules.Gambling
 {
@@ -8,13 +12,34 @@ namespace WizBot.Modules.Gambling
     {
         public class CurrencyRaffleCommands : WizBotSubmodule<CurrencyRaffleService>
         {
+            private readonly IBotConfigProvider _bc;
+
+            public CurrencyRaffleCommands(IBotConfigProvider bc)
+            {
+                _bc = bc;
+            }
             [WizBotCommand, Usage, Description, Aliases]
             public async Task RaffleCur(int amount)
             {
-                if (_service.Games.TryAdd(Context.Channel.Id,
-                    ))
+                async Task OnEnded(IUser arg, int won)
                 {
+                    await ReplyConfirmLocalized("rafflecur_ended", _bc.BotConfig.CurrencyName, Format.Bold(arg.ToString()), won + _bc.BotConfig.CurrencySign);
+                }
+                var res = await _service.JoinOrCreateGame(Context.Channel.Id,
+                    Context.User, amount, OnEnded)
+                        .ConfigureAwait(false);
 
+                if (res.Item1 != null)
+                {
+                    await Context.Channel.SendConfirmAsync(GetText("rafflecur_joined", Context.User.ToString()),
+                        string.Join("\n", res.Item1.Users)).ConfigureAwait(false);
+                }
+                else
+                {
+                    if (res.Item2 == CurrencyRaffleService.JoinErrorType.AlreadyJoined)
+                        await ReplyErrorLocalized("rafflecur_already_joined").ConfigureAwait(false);
+                    else if (res.Item2 == CurrencyRaffleService.JoinErrorType.NotEnoughCurrency)
+                        await ReplyErrorLocalized("not_enough").ConfigureAwait(false);
                 }
             }
         }
