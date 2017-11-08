04acb3566968079e541e973fd1ad4e7fafd0b0bc
Wizkiller96 <wizkiller96@hotmail.com>
2017-11-08 02:52:34 -0500

    .rlr - Set a level requirement on a self-assignable role

--- WizBot.Core/Migrations/20171107131810_sar-level-req.Designer.cs
+++ WizBot.Core/Migrations/20171107131810_sar-level-req.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace WizBot.Migrations
 {
     [DbContext(typeof(WizBotContext))]
-    partial class WizBotSqliteContextModelSnapshot : ModelSnapshot
+    [Migration("20171107131810_sar-level-req")]
+    partial class sarlevelreq
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
