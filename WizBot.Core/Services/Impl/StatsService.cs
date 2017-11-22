246fdc69d4dc66f5e9119161c6349d66111a67c8
Wizkiller96 <wizkiller96@hotmail.com>
2017-11-22 09:17:28 -0500

    Fixed multiple responses?

--- WizBot.Core/Services/Impl/StatsService.cs
+++ WizBot.Core/Services/Impl/StatsService.cs
@@ -21,7 +21,7 @@ namespace WizBot.Core.Services.Impl
         private readonly IBotCredentials _creds;
         private readonly DateTime _started;
 
-        public const string BotVersion = "2.5.7";
+        public const string BotVersion = "2.5.8";
 
         public string Author => "Kwoth#2560\nWizkiller96#2947";
         public string Library => "Discord.Net";
