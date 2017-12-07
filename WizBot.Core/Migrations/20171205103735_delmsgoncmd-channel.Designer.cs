1f955237eff7fc4600bc716a2f9c8a617c46690f
Wizkiller96 <wizkiller96@hotmail.com>
2017-12-07 16:43:23 -0500

    .delmsgoncmd vastly improved. You can enable/disable it per channel, and list current status. Use .h .delmsgoncmd for more info

--- WizBot.Core/Migrations/20171205103735_delmsgoncmd-channel.Designer.cs
+++ WizBot.Core/Migrations/20171205103735_delmsgoncmd-channel.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace WizBot.Migrations
 {
     [DbContext(typeof(WizBotContext))]
-    partial class WizBotSqliteContextModelSnapshot : ModelSnapshot
+    [Migration("20171205103735_delmsgoncmd-channel")]
+    partial class delmsgoncmdchannel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
