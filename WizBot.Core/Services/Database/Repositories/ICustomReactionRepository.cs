98e2a259aa8e9e4ada8bc08fe304bbf62453e079
Wizkiller96 <wizkiller96@hotmail.com>
2018-04-06 22:10:22 -0400

    This should make custom reactions use way less memory on bigger bots

--- WizBot.Core/Services/Database/Repositories/ICustomReactionRepository.cs
+++ WizBot.Core/Services/Database/Repositories/ICustomReactionRepository.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using WizBot.Core.Services.Database.Models;
 
 namespace WizBot.Core.Services.Database.Repositories
 {
     public interface ICustomReactionRepository : IRepository<CustomReaction>
     {
-
+        CustomReaction[] GetGlobalAndFor(IEnumerable<long> ids);
+        CustomReaction[] For(ulong id);
     }
 }
