d046b8666b4a275a6cd74b5a68687d77f8049b41
Wizkiller96 <wizkiller96@hotmail.com>
2018-04-28 10:01:18 -0400

    Just tried someting out, commented out

--- WizBot.Core/Modules/Games/Games.cs
+++ WizBot.Core/Modules/Games/Games.cs
@@ -8,6 +8,8 @@ using WizBot.Common.Attributes;
 using WizBot.Extensions;
 using WizBot.Modules.Games.Common;
 using WizBot.Modules.Games.Services;
+using System.Collections.Generic;
+using Discord.WebSocket;
 
 namespace WizBot.Modules.Games
 {
@@ -52,6 +54,150 @@ namespace WizBot.Modules.Games
         //            }
         //        }
         //#endif
+
+        //public class Gugl
+        //{
+        //    public string Text { get; set; }
+        //    public Gugl Parent { get; set; }
+        //    public List<Gugl> Options { get; set; } = new List<Gugl>();
+
+        //    public Gugl Add(params Gugl[] gs)
+        //    {
+        //        Options.AddRange(gs);
+        //        gs.ForEach(x => x.Parent = this);
+        //        return this;
+        //    }
+        //}
+
+        //public class Stuff
+        //{
+        //    public Gugl Current { get; set; }
+        //    public ushort Index { get; set; } = 0;
+
+        //    public void Up()
+        //    {
+        //        if (Index == 0)
+        //            return;
+        //        Index--;
+        //    }
+
+        //    public void Down()
+        //    {
+        //        if (Index >= Current.Options.Count)
+        //            return;
+        //        Index++;
+        //    }
+
+        //    public void Ok()
+        //    {
+        //        if (Current != null && Index < Current.Options.Count)
+        //            Current = Current.Options[Index];
+        //    }
+
+        //    public void Back()
+        //    {
+        //        if (Current.Parent != null)
+        //            Current = Current.Parent;
+        //    }
+
+        //    public EmbedBuilder GetEmbed()
+        //    {
+        //        var eb = new EmbedBuilder()
+        //            .WithTitle(Current.Text);
+        //        var str = "";
+        //        for (int i = 0; i < Current.Options.Count; i++)
+        //        {
+        //            var op = Current.Options[i];
+        //            if (Index == i)
+        //                str += "-> ";
+        //            str += $"`{i + 1}.` {Format.Bold(op.Text)}\n";
+        //        }
+        //        if (!string.IsNullOrWhiteSpace(str))
+        //        {
+        //            eb.WithDescription(str);
+        //        }
+        //        return eb;
+        //    }
+        //}
+
+        //[WizBotCommand, Usage, Description, Aliases]
+        //public async Task gugl()
+        //{
+        //    var _1 = new Gugl()
+        //    {
+        //        Text = "Does this work?",
+        //    };
+
+        //    _1.Add(new Gugl()
+        //    {
+        //        Text = "No",
+        //        Parent = _1
+        //    }.Add(new Gugl()
+        //    {
+        //        Text = "Well, too bad."
+        //    }), new Gugl()
+        //    {
+        //        Text = "Yes",
+        //        Parent = _1
+        //    }.Add(new Gugl()
+        //    {
+        //        Text = "Insult google."
+        //    }.Add(new Gugl() { Text = "Google u sux uwu" }),
+        //    new Gugl()
+        //    {
+        //        Text = "Praise google."
+        //    }.Add(new Gugl() { Text = "Google u rox uwu" })));
+
+
+        //    var menu = new Stuff() { Current = _1 };
+
+        //    var msg = await Context.Channel.EmbedAsync(menu.GetEmbed());
+        //    var back = new Emoji("⬅");
+        //    var up = new Emoji("⬆");
+        //    var down = new Emoji("⬇");
+        //    var ok = new Emoji("🔵");
+
+        //    await Task.WhenAll(msg.AddReactionAsync(back),
+        //        msg.AddReactionAsync(up),
+        //        msg.AddReactionAsync(down),
+        //        msg.AddReactionAsync(ok));
+
+        //    msg.OnReaction((DiscordSocketClient)Context.Client, async (x) =>
+        //    {
+        //        if (x.UserId != Context.User.Id)
+        //            return;
+        //        try
+        //        {
+        //            if (x.Emote.Name == back.Name)
+        //            {
+        //                menu.Back();
+        //                await msg.ModifyAsync(y => y.Embed = menu.GetEmbed().Build());
+        //            }
+        //            else if (x.Emote.Name == up.Name)
+        //            {
+        //                menu.Up();
+        //                await msg.ModifyAsync(y => y.Embed = menu.GetEmbed().Build());
+        //            }
+        //            else if (x.Emote.Name == down.Name)
+        //            {
+        //                menu.Down();
+        //                await msg.ModifyAsync(y => y.Embed = menu.GetEmbed().Build());
+        //            }
+        //            else if (x.Emote.Name == ok.Name)
+        //            {
+        //                menu.Ok();
+        //                await msg.ModifyAsync(y => y.Embed = menu.GetEmbed().Build());
+        //            }
+        //        }
+        //        finally
+        //        {
+        //            if (x.User.IsSpecified)
+        //                await msg.RemoveReactionAsync(x.Emote, x.User.Value);
+        //        }
+        //    });
+
+        //}
+
         [WizBotCommand, Usage, Description, Aliases]
         public async Task Choose([Remainder] string list = null)
         {
