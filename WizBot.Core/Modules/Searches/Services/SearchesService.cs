2344e1d843ae8084232c47a130e7f68a707ed829
Wizkiller96 <wizkiller96@hotmail.com>
2018-07-16 08:53:21 -0400

    You can now specify xp bar starting points, length and color in xp_template.json, font change to noto-sans as whiteney is apparently not free

--- WizBot.Core/Modules/Searches/Services/SearchesService.cs
+++ WizBot.Core/Modules/Searches/Services/SearchesService.cs
@@ -201,7 +201,7 @@ namespace WizBot.Modules.Searches.Services
                     WrapTextWidth = 190,
                 },
                 text,
-                _fonts.WhiteneyBold.CreateFont(20),
+                _fonts.NotoSans.CreateFont(20),
                 Rgba32.Black,
                 new PointF(25, 225)));
 
