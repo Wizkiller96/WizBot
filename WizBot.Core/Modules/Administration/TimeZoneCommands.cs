2eb8a6c8d1f4b14d7b9edd124ea62ed4cdffda0f
Wizkiller96 <wizkiller96@hotmail.com>
2018-07-22 15:51:16 -0400

    Added description for every single BotConfigEdit type, fixed .timezone not requiring any permissions to set

--- WizBot.Core/Modules/Administration/TimeZoneCommands.cs
+++ WizBot.Core/Modules/Administration/TimeZoneCommands.cs
@@ -32,20 +32,22 @@ namespace WizBot.Modules.Administration
                     (curPage) => new EmbedBuilder()
                         .WithOkColor()
                         .WithTitle(GetText("timezones_available"))
-                        .WithDescription(string.Join("\n", timezones.Skip(curPage * timezonesPerPage).Take(timezonesPerPage).Select(x => $"`{x.Id,-25}` {(x.BaseUtcOffset < TimeSpan.Zero? "-" : "+")}{x.BaseUtcOffset:hhmm}"))),
+                        .WithDescription(string.Join("\n", timezones.Skip(curPage * timezonesPerPage).Take(timezonesPerPage).Select(x => $"`{x.Id,-25}` {(x.BaseUtcOffset < TimeSpan.Zero ? "-" : "+")}{x.BaseUtcOffset:hhmm}"))),
                     timezones.Length, timezonesPerPage).ConfigureAwait(false);
             }
 
             [WizBotCommand, Usage, Description, Aliases]
             [RequireContext(ContextType.Guild)]
-            public async Task Timezone([Remainder] string id = null)
+            public async Task Timezone()
             {
-                if (string.IsNullOrWhiteSpace(id))
-                {
-                    await ReplyConfirmLocalized("timezone_guild", _service.GetTimeZoneOrUtc(Context.Guild.Id)).ConfigureAwait(false);
-                    return;
-                }
+                await ReplyConfirmLocalized("timezone_guild", _service.GetTimeZoneOrUtc(Context.Guild.Id)).ConfigureAwait(false);
+            }
 
+            [WizBotCommand, Usage, Description, Aliases]
+            [RequireContext(ContextType.Guild)]
+            [RequireUserPermission(GuildPermission.Administrator)]
+            public async Task Timezone([Remainder] string id)
+            {
                 TimeZoneInfo tz;
                 try { tz = TimeZoneInfo.FindSystemTimeZoneById(id); } catch { tz = null; }
 
