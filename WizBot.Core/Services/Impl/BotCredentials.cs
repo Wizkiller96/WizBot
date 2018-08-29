43791d29959583bcc58768b13308730e2c605892
Wizkiller96 <wizkiller96@hotmail.com>
2018-08-29 03:41:31 -0400

    refactor the default for RedisOptions

--- WizBot.Core/Services/Impl/BotCredentials.cs
+++ WizBot.Core/Services/Impl/BotCredentials.cs
@@ -45,7 +45,7 @@ namespace WizBot.Core.Services.Impl
         public string VotesUrl { get; }
         public string VotesToken { get; }
         public string BotListToken { get; }
-        public string RedisOptions { get; }
+        public string RedisOptions { get; } = "127.0.0.1,syncTimeout=3000";
 
         public BotCredentials()
         {
@@ -83,7 +83,8 @@ namespace WizBot.Core.Services.Impl
                 CleverbotApiKey = data[nameof(CleverbotApiKey)];
                 MiningProxyUrl = data[nameof(MiningProxyUrl)];
                 MiningProxyCreds = data[nameof(MiningProxyCreds)];
-                RedisOptions = data[nameof(RedisOptions)];
+                if (!string.IsNullOrWhiteSpace(data[nameof(RedisOptions)]))
+                    RedisOptions = data[nameof(RedisOptions)];
 
                 VotesToken = data[nameof(VotesToken)];
                 VotesUrl = data[nameof(VotesUrl)];
