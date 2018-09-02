486f7400eeff9ed7463c3dece64edbe1bdf58ee0
Wizkiller96 <wizkiller96@hotmail.com>
2018-09-02 01:16:58 -0400

    Fixed a bug with .waifugift

--- WizBot.Core/Modules/Gambling/Services/WaifuService.cs
+++ WizBot.Core/Modules/Gambling/Services/WaifuService.cs
@@ -336,7 +336,8 @@ namespace WizBot.Modules.Gambling.Services
 
             using (var uow = _db.UnitOfWork)
             {
-                var w = uow.Waifus.ByWaifuUserId(giftedWaifu.Id, set => set.Include(x => x.Items));
+                var w = uow.Waifus.ByWaifuUserId(giftedWaifu.Id, set => set.Include(x => x.Items)
+                    .Include(x => x.Claimer));
                 if (w == null)
                 {
                     uow.Waifus.Add(w = new WaifuInfo()
