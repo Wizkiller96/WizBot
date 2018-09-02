f0d2f20dc8a83688beade58c8a196e0db74a76f4
Wizkiller96 <wizkiller96@hotmail.com>
2018-09-02 01:05:33 -0400

    Mining payout fixed

---
+++ WizBot.Core/Modules/Gambling/WebMiningCommands.cs
@@ -0,0 +1,12 @@
+using WizBot.Core.Modules.Gambling.Services;
+
+namespace WizBot.Modules.Gambling
+{
+    public partial class Gambling
+    {
+        public class WebMiningCommands : WizBotSubmodule<WebMiningService>
+        {
+
+        }
+    }
+}
