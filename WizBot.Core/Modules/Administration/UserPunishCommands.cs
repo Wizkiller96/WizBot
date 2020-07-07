512022d8c28bca52e3be92ccf584735fe1b12fd6
Wizkiller96 <wizkiller96@hotmail.com>
2020-07-07 13:11:15 -0400

    punishment name triggered by .warn should now be shown properly

--- WizBot.Core/Modules/Administration/UserPunishCommands.cs
+++ WizBot.Core/Modules/Administration/UserPunishCommands.cs
@@ -79,7 +79,7 @@ namespace WizBot.Modules.Administration
                 }
                 else
                 {
-                    await ReplyConfirmLocalizedAsync("user_warned_and_punished", Format.Bold(user.ToString()), Format.Bold(punishment.ToString())).ConfigureAwait(false);
+                    await ReplyConfirmLocalizedAsync("user_warned_and_punished", Format.Bold(user.ToString()), Format.Bold(punishment.Punishment.ToString())).ConfigureAwait(false);
                 }
             }
 
