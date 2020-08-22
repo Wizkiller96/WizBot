f434762be6f24a344cec4d807b244c9b10ede2d0
Wizkiller96 <wizkiller96@hotmail.com>
2020-08-22 17:05:54 -0400

    1 slot per second

--- WizBot.Core/Modules/Gambling/SlotCommands.cs
+++ WizBot.Core/Modules/Gambling/SlotCommands.cs
@@ -223,7 +223,7 @@ namespace WizBot.Modules.Gambling
                 {
                     var _ = Task.Run(async () =>
                     {
-                        await Task.Delay(1500).ConfigureAwait(false);
+                        await Task.Delay(1000).ConfigureAwait(false);
                         _runningUsers.Remove(ctx.User.Id);
                     });
                 }
