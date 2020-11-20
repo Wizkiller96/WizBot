04c37715d440d59e4cb681ef42e9b702c9df0e50
Wizkiller96 <wizkiller96@hotmail.com>
2020-11-20 18:12:43 -0500

    Mark .warnp softbans too

--- WizBot.Core/Modules/Administration/Services/UserPunishService.cs
+++ WizBot.Core/Modules/Administration/Services/UserPunishService.cs
@@ -108,7 +108,7 @@ namespace WizBot.Modules.Administration.Services
                             await _mute.TimedBan(user, TimeSpan.FromMinutes(p.Time), "Warned too many times.").ConfigureAwait(false);
                         break;
                     case PunishmentAction.Softban:
-                        await guild.AddBanAsync(user, 7, reason: "Warned too many times").ConfigureAwait(false);
+                        await guild.AddBanAsync(user, 7, reason: "Softban | Warned too many times").ConfigureAwait(false);
                         try
                         {
                             await guild.RemoveBanAsync(user).ConfigureAwait(false);
