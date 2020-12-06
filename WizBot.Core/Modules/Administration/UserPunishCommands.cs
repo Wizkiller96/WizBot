d16baa8a762c168d8099f0271ebddd25f15ef12f
Wizkiller96 <wizkiller96@hotmail.com>
2020-12-06 06:56:53 -0500

    Rename time to duration.

--- WizBot.Core/Modules/Administration/UserPunishCommands.cs
+++ WizBot.Core/Modules/Administration/UserPunishCommands.cs
@@ -382,7 +382,7 @@ namespace WizBot.Modules.Administration
                     .WithTitle("⛔️ " + GetText("banned_user"))
                     .AddField(efb => efb.WithName(GetText("username")).WithValue(user.ToString()).WithIsInline(true))
                     .AddField(efb => efb.WithName("ID").WithValue(user.Id.ToString()).WithIsInline(true))
-                    .AddField(efb => efb.WithName(GetText("time")).WithValue($"{time.Time.Days}d {time.Time.Hours}h {time.Time.Minutes}m").WithIsInline(true));
+                    .AddField(efb => efb.WithName(GetText("duration")).WithValue($"{time.Time.Days}d {time.Time.Hours}h {time.Time.Minutes}m").WithIsInline(true));
 
                 if (dmFailed)
                 {
