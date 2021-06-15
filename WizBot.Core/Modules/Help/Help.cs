41349f688dc40519218145d157f3c45349ffc104
Wizkiller96 <wizkiller96@hotmail.com>
2021-06-15 11:39:18 -0400

    .gencmdlist should now prepend versions to the file

--- WizBot.Core/Modules/Help/Help.cs
+++ WizBot.Core/Modules/Help/Help.cs
@@ -371,11 +371,13 @@ namespace WizBot.Modules.Help
                 if (!versionList.Contains(StatsService.BotVersion))
                 {
                     // save the file with new version added
-                    versionList.Add(StatsService.BotVersion);
-                    versionListString = System.Text.Json.JsonSerializer.Serialize(versionList, new JsonSerializerOptions()
-                    {
-                        WriteIndented = true
-                    });
+                    // versionList.Add(StatsService.BotVersion);
+                    versionListString = System.Text.Json.JsonSerializer.Serialize(
+                        versionList.Prepend(StatsService.BotVersion),
+                        new JsonSerializerOptions()
+                        {
+                            WriteIndented = true
+                        });
                     await File.WriteAllTextAsync(cmdVersionsFilePath, versionListString);
                     
                     // upload the updated version list
