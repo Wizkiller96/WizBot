1af41df1d06039fbb81d094d060955f51a4a63ab
Wizkiller96 <wizkiller96@hotmail.com>
2021-07-20 15:10:40 -0400

    Version upped to 2.46.4

--- WizBot.Core/Services/Impl/StatsService.cs
+++ WizBot.Core/Services/Impl/StatsService.cs
@@ -18,7 +18,7 @@ namespace WizBot.Core.Services.Impl
         private readonly IBotCredentials _creds;
         private readonly DateTime _started;
 
-        public const string BotVersion = "2.46.2";
+        public const string BotVersion = "2.46.4";
         public string Author => "Kwoth#2452\nWizkiller96#5074";
         public string Library => "Discord.Net";
 
