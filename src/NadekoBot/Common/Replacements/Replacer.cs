1ac472c6766820703134a7c1e65abb4d4f4148bc
Kwoth <master.kwoth@gmail.com>
2021-09-27 10:52:14 +0200

    Urls will now properly work in embeds. Fixes #306

--- src/NadekoBot/Common/Replacements/Replacer.cs
+++ src/NadekoBot/Common/Replacements/Replacer.cs
@@ -53,6 +53,7 @@ namespace NadekoBot.Common.Replacements
             newEmbedData.Title = Replace(embedData.Title);
             newEmbedData.Thumbnail = Replace(embedData.Thumbnail);
             newEmbedData.Image = Replace(embedData.Image);
+            newEmbedData.Url = Replace(embedData.Url);
             if (embedData.Author != null)
             {
                 newEmbedData.Author = new SmartTextEmbedAuthor();
