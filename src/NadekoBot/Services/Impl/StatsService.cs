dc344caec69f5189a278d0f4016448c5bda9868a
Kwoth <master.kwoth@gmail.com>
2021-11-03 15:23:45 +0100

    Version upped to 3.0.8

--- src/NadekoBot/Services/Impl/StatsService.cs
+++ src/NadekoBot/Services/Impl/StatsService.cs
@@ -20,7 +20,7 @@ namespace NadekoBot.Services
         private readonly IBotCredentials _creds;
         private readonly DateTime _started;
 
-        public const string BotVersion = "3.0.7";
+        public const string BotVersion = "3.0.8";
         public string Author => "Kwoth#2452";
         public string Library => "Discord.Net";
         public double MessagesPerSecond => MessageCounter / GetUptime().TotalSeconds;
