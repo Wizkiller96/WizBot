9e8f9f6cd5cd7a984b30ffee4c659e952170e38a
Wizkiller96 <wizkiller96@hotmail.com>
2021-11-15 10:14:41 -0500

    Version upped to 3.0.8

--- src/WizBot/Services/Impl/StatsService.cs
+++ src/WizBot/Services/Impl/StatsService.cs
@@ -20,7 +20,7 @@ namespace WizBot.Services
         private readonly IBotCredentials _creds;
         private readonly DateTime _started;
 
-        public const string BotVersion = "3.0.7";
+        public const string BotVersion = "3.0.8";
         public string Author => "Kwoth#2452\nWizkiller96#5074";
         public string Library => "Discord.Net";
         public double MessagesPerSecond => MessageCounter / GetUptime().TotalSeconds;
