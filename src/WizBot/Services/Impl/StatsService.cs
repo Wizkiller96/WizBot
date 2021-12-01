8cf0cab21114225abdc24d9a5cdf9309f20a287b
Wizkiller96 <wizkiller96@hotmail.com>
2021-12-01 05:09:07 -0500

    Fixed some console log spam which was incorrectly done by streamrole feature, upped version to 3.0.10 in stats

--- src/WizBot/Services/Impl/StatsService.cs
+++ src/WizBot/Services/Impl/StatsService.cs
@@ -20,7 +20,7 @@ namespace WizBot.Services
         private readonly IBotCredentials _creds;
         private readonly DateTime _started;
 
-        public const string BotVersion = "3.0.9";
+        public const string BotVersion = "3.0.10";
         public string Author => "Kwoth#2452\nWizkiller96#5074";
         public string Library => "Discord.Net";
         public double MessagesPerSecond => MessageCounter / GetUptime().TotalSeconds;
