352ec9a562479bd5391a4657e3a44ded2fb65b82
Kwoth <master.kwoth@gmail.com>
2022-01-06 21:05:55 +0100

    Usings https in gelbooru

--- src/NadekoBot/Modules/Nsfw/_Common/Downloaders/GelbooruImageDownloader.cs
+++ src/NadekoBot/Modules/Nsfw/_Common/Downloaders/GelbooruImageDownloader.cs
@@ -18,8 +18,13 @@ public class GelbooruImageDownloader : ImageDownloader<DapiImageObject>
         CancellationToken cancel = default)
     {
         var tagString = ImageDownloaderHelper.GetTagString(tags, isExplicit);
-        var uri =
-            $"http://gelbooru.com/index.php?page=dapi&s=post&json=1&q=index&limit=100&tags={tagString}&pid={page}";
+        var uri = $"https://gelbooru.com/index.php?page=dapi"
+                  + $"&s=post"
+                  + $"&json=1"
+                  + $"&q=index"
+                  + $"&limit=100"
+                  + $"&tags={tagString}"
+                  + $"&pid={page}";
         using var req = new HttpRequestMessage(HttpMethod.Get, uri);
         using var res = await _http.SendAsync(req, cancel);
         res.EnsureSuccessStatusCode();
