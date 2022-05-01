e437ddba898f62269076ab761261eda8fc51dafb
Wizkiller96 <wizkiller96@hotmail.com>
2022-04-30 22:59:36 -0400

    Forgot to commit a file.

---
+++ src/WizBot/Db/Models/BankUser.cs
@@ -0,0 +1,9 @@
+﻿using WizBot.Services.Database.Models;
+
+namespace WizBot.Db.Models;
+
+public class BankUser : DbEntity
+{
+    public ulong UserId { get; set; }
+    public long Balance { get; set; }
+}
