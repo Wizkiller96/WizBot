9a21ba3d53e450e05c96ea8a749aaf26171584f4
Kwoth <master.kwoth@gmail.com>
2022-07-20 23:47:57 +0200

    Experimenting with new deck

---
+++ src/Nadeko.Econ/NewDeck.cs
@@ -0,0 +1,35 @@
+﻿namespace Nadeko.Econ;
+
+public abstract class NewDeck<TCard, TSuit> 
+    where TCard: NewCard<TSuit> 
+    where TSuit : Enum
+{
+    public int CurrentCount { get; }
+    public int TotalCount { get; }
+
+    public abstract TCard Draw();
+}
+
+public abstract class NewCard<TSuit>
+    where TSuit: Enum
+{
+    
+}
+
+public sealed class RegularCard : NewCard<RegularSuit>
+{
+    
+}
+
+public enum RegularSuit
+{
+    
+}
+
+public sealed class RegularDeck : NewDeck<RegularCard, RegularSuit>
+{
+    public override RegularCard Draw()
+        => throw new NotImplementedException();
+}
+
+
