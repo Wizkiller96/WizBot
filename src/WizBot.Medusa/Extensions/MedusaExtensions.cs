9cbc4f5d2b33fcade15082b532226611bbc357a3
Wizkiller96 <wizkiller96@hotmail.com>
2022-07-31 19:42:13 -0400

    Fixed medusa Reply*LocalizedAsync not working with placeholders

--- src/WizBot.Medusa/Extensions/MedusaExtensions.cs
+++ src/WizBot.Medusa/Extensions/MedusaExtensions.cs
@@ -47,7 +47,7 @@ public static class MedusaExtensions
         => ctx.Message.AddReactionAsync(new Emoji("🤔"));
     
     public static Task<IUserMessage> ErrorLocalizedAsync(this AnyContext ctx, string key, params object[]? args)
-        => ctx.SendErrorAsync(ctx.GetText(key));
+        => ctx.SendErrorAsync(ctx.GetText(key, args));
 
     public static Task<IUserMessage> PendingLocalizedAsync(this AnyContext ctx, string key, params object[]? args)
         => ctx.SendPendingAsync(ctx.GetText(key, args));
@@ -56,11 +56,11 @@ public static class MedusaExtensions
         => ctx.SendConfirmAsync(ctx.GetText(key, args));
 
     public static Task<IUserMessage> ReplyErrorLocalizedAsync(this AnyContext ctx, string key, params object[]? args)
-        => ctx.SendErrorAsync($"{Format.Bold(ctx.User.ToString())} {ctx.GetText(key)}");
+        => ctx.SendErrorAsync($"{Format.Bold(ctx.User.ToString())} {ctx.GetText(key, args)}");
 
     public static Task<IUserMessage> ReplyPendingLocalizedAsync(this AnyContext ctx, string key, params object[]? args)
-        => ctx.SendPendingAsync($"{Format.Bold(ctx.User.ToString())} {ctx.GetText(key)}");
+        => ctx.SendPendingAsync($"{Format.Bold(ctx.User.ToString())} {ctx.GetText(key, args)}");
 
     public static Task<IUserMessage> ReplyConfirmLocalizedAsync(this AnyContext ctx, string key, params object[]? args)
-        => ctx.SendConfirmAsync($"{Format.Bold(ctx.User.ToString())} {ctx.GetText(key)}");
+        => ctx.SendConfirmAsync($"{Format.Bold(ctx.User.ToString())} {ctx.GetText(key, args)}");
 }
