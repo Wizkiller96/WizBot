f4974f278dea23e30abc2c2a3ee8f1c4ece64cd8
Wizkiller96 <wizkiller96@hotmail.com>
2022-10-15 14:03:00 -0400

    Medusa modules (sneks) should now inherit medusa description when listed in .mdls command

--- src/WizBot/Modules/Help/Help.cs
+++ src/WizBot/Modules/Help/Help.cs
@@ -92,7 +92,7 @@ public partial class Help : WizBotModule<HelpService>
                 localModules.OrderBy(module => module.Name)
                             .ToList()
                             .ForEach(module => embed.AddField($"{GetModuleEmoji(module.Name)} {module.Name}",
-                                GetText(GetModuleLocStr(module.Name))
+                                GetModuleDescription(module.Name)
                                 + "\n"
                                 + Format.Code(GetText(strs.module_footer(prefix, module.Name.ToLowerInvariant()))),
                                 true));
@@ -102,6 +102,25 @@ public partial class Help : WizBotModule<HelpService>
             topLevelModules.Count(),
             12,
             false);
+    }
+    
+    private string GetModuleDescription(string moduleName)
+    {
+        var key = GetModuleLocStr(moduleName);
+
+        if (key.Key == strs.module_description_missing.Key)
+        {
+            var desc = _medusae
+                       .GetLoadedMedusae(Culture)
+                       .FirstOrDefault(m => m.Sneks
+                                             .Any(x => x.Name.Equals(moduleName, StringComparison.InvariantCultureIgnoreCase)))
+                       ?.Description;
+
+            if (desc is not null)
+                return desc;
+        }
+
+        return GetText(key);
     }
 
     private LocStr GetModuleLocStr(string moduleName)
