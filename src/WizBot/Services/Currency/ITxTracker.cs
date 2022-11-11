1e1b2f2d9e6fe11c07814c2073ece7c7a8a962e7
Wizkiller96 <wizkiller96@hotmail.com>
2022-11-11 10:50:16 -0500

    Fixed nullref in xploop

--- src/WizBot/Services/Currency/ITxTracker.cs
+++ src/WizBot/Services/Currency/ITxTracker.cs
@@ -4,6 +4,6 @@ namespace WizBot.Services;
 
 public interface ITxTracker
 {
-    Task TrackAdd(long amount, TxData txData);
-    Task TrackRemove(long amount, TxData txData);
+    Task TrackAdd(long amount, TxData? txData);
+    Task TrackRemove(long amount, TxData? txData);
 }
