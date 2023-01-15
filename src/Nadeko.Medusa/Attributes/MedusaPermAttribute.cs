737bbb8ec141fbd30d667dee6c4af588b9737fa4
Kwoth <master.kwoth@gmail.com>
2023-01-15 04:07:04 +0100

    Added bot_perm support to medusa system

---
+++ src/Nadeko.Medusa/Attributes/MedusaPermAttribute.cs
@@ -0,0 +1,10 @@
+namespace Nadeko.Snake;
+
+/// <summary>
+/// Used as a marker class for bot_perm and user_perm Attributes
+/// Has no functionality.
+/// </summary>
+public abstract class MedusaPermAttribute : Attribute
+{
+    
+}
