211ee685fd1f5b969cdb5b4c88670ae8335dc4ef
Wizkiller96 <wizkiller96@hotmail.com>
2023-02-13 10:07:41 -0500

    Nhentai removed as it's currently unfixable

--- src/WizBot/Modules/Nsfw/Nhentai/INhentaiService.cs
+++ src/WizBot/Modules/Nsfw/Nhentai/INhentaiService.cs
@@ -1,9 +1,9 @@
-﻿using WizBot.Modules.Searches.Common;
-
-namespace WizBot.Modules.Nsfw;
-
-public interface INhentaiService
-{
-    Task<Gallery?> GetAsync(uint id);
-    Task<IReadOnlyList<uint>> GetIdsBySearchAsync(string search);
-}
+﻿// using WizBot.Modules.Searches.Common;
+//
+// namespace WizBot.Modules.Nsfw;
+//
+// public interface INhentaiService
+// {
+//     Task<Gallery?> GetAsync(uint id);
+//     Task<IReadOnlyList<uint>> GetIdsBySearchAsync(string search);
+// }
