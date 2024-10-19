084cdf0bdc3f835808f687dd9eb8d835472afde5
Wizkiller96 <wizkiller96@hotmail.com>
2024-10-19 16:01:37 -0400

    dev: added botonguild api endpoint

--- src/WizBot/Services/GrpcApi/OtherSvc.cs
+++ src/WizBot/Services/GrpcApi/OtherSvc.cs
@@ -38,6 +38,19 @@ public sealed class OtherSvc : GrpcOther.GrpcOtherBase, INService
         _stats = stats;
         _cache = cache;
     }
+    
+    [GrpcNoAuthRequired]
+    public override async Task<BotOnGuildReply> BotOnGuild(BotOnGuildRequest request, ServerCallContext context)
+    {
+        var guild = await _client.GetGuildAsync(request.GuildId);
+        
+        var reply = new BotOnGuildReply
+        {
+            Success = guild is not null
+        };
+        
+        return reply;
+    }
 
     public override async Task<GetTextChannelsReply> GetTextChannels(
         GetTextChannelsRequest request,
