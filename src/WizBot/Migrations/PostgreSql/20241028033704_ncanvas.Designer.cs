828579206b863bd83972ef20f253536d1de8d7dd
Wizkiller96 <wizkiller96@hotmail.com>
2024-10-29 23:50:02 -0400

    fix: Fixed error in pipeline.

--- src/WizBot/Migrations/PostgreSql/20241028033704_ncanvas.Designer.cs
+++ src/WizBot/Migrations/PostgreSql/20241028033704_ncanvas.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using NadekoBot.Db;
+using WizBot.Db;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
