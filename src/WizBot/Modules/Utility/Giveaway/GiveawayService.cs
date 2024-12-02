280a10601bdac3f8b12d805cbf4c16aa3630c3e1
Wizkiller96 <wizkiller96@hotmail.com>
2024-12-01 19:13:48 -0500

    change: Finished giveaway will now reply to the giveaway message and ping a winner

--- src/WizBot/Modules/Utility/Giveaway/GiveawayService.cs
+++ src/WizBot/Modules/Utility/Giveaway/GiveawayService.cs
@@ -341,6 +341,9 @@ public sealed class GiveawayService : INService, IReadyExecutor
         try
         {
             await msg.ModifyAsync(x => x.Embed = eb.Build());
+            
+            if (winner is not null)
+                await _sender.Response(ch).Message(msg).Text($"🎉 <@{winner.UserId}>").SendAsync();
         }
         catch
         {
