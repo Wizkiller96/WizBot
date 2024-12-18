b79f9f60226c216bc9c885cb055c88b04b764d45
Wizkiller96 <wizkiller96@hotmail.com>
2024-12-18 05:32:30 -0500

    change: Queueing a song after the queue is finished will restart the playback

--- src/WizBot/Modules/Music/_common/Impl/MusicPlayer.cs
+++ src/WizBot/Modules/Music/_common/Impl/MusicPlayer.cs
@@ -65,7 +65,17 @@ public sealed class MusicPlayer : IMusicPlayer
 
         _songBuffer = new PoopyBufferImmortalized(_vc.InputLength);
 
-        _thread = new(async () => { await PlayLoop(); });
+        _thread = new(async () =>
+        {
+            try
+            {
+                await PlayLoop();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Music player thread crashed");
+            }
+        });
         _thread.Start();
     }
 
@@ -401,13 +411,25 @@ public sealed class MusicPlayer : IMusicPlayer
         var song = await _trackResolveProvider.QuerySongAsync(query, forcePlatform);
         if (song is null)
             return default;
+        
 
-        int index;
+        var wasLast = _queue.IsLast();
 
-        if (asNext)
-            return (_queue.EnqueueNext(song, queuer, out index), index);
+        try
+        {
+            int index;
+            if (asNext)
+                return (_queue.EnqueueNext(song, queuer, out index), index);
 
-        return (_queue.Enqueue(song, queuer, out index), index);
+            return (_queue.Enqueue(song, queuer, out index), index);
+        }
+        finally
+        {
+            // if (wasLast && IsStopped)
+            // {
+            //     IsStopped = false;
+            // }
+        }
     }
 
     public async Task EnqueueManyAsync(IEnumerable<(string Query, MusicPlatform Platform)> queries, string queuer)
