6079ae7c0fc42245e2643de026f2fc6f1ef26a2b
Wizkiller96 <wizkiller96@hotmail.com>
2025-04-12 02:11:27 -0400

    fix: fixed questcommands showing up as a module

--- src/WizBot/Modules/Games/Quests/QuestCommands.cs
+++ src/WizBot/Modules/Games/Quests/QuestCommands.cs
@@ -1,38 +1,43 @@
-﻿namespace WizBot.Modules.Games.Quests;
+﻿using WizBot.Modules.Games.Quests;
 
-public class QuestCommands : WizModule<QuestService>
+namespace WizBot.Modules.Games;
+
+public partial class Games
 {
-    [Cmd]
-    public async Task QuestLog()
+    public class QuestCommands : WizModule<QuestService>
     {
-        var now = DateTime.UtcNow;
-        var quests = await _service.GetUserQuestsAsync(ctx.User.Id, now);
-
-        var embed = CreateEmbed()
-            .WithOkColor()
-            .WithTitle(GetText(strs.quest_log));
-
-        var allDone = quests.All(x => x.UserQuest.IsCompleted);
-
-        var tmrw = now.AddDays(1).Date;
-        var desc = GetText(strs.dailies_reset(TimestampTag.FromDateTime(tmrw, TimestampTagStyles.Relative)));
-        if (allDone)
-            desc = GetText(strs.dailies_done) + "\n" + desc;
-
-        embed.WithDescription(desc);
-
-        foreach (var res in quests)
+        [Cmd]
+        public async Task QuestLog()
         {
-            if (res.Quest is null)
-                continue;
+            var now = DateTime.UtcNow;
+            var quests = await _service.GetUserQuestsAsync(ctx.User.Id, now);
 
-            embed.AddField(
-                (res.UserQuest.IsCompleted ? IQuest.COMPLETED : IQuest.INCOMPLETE) + " " + res.Quest.Name,
-                $"{res.Quest.Desc}\n\n" +
-                res.Quest.ToString(res.UserQuest.Progress),
-                true);
+            var embed = CreateEmbed()
+                .WithOkColor()
+                .WithTitle(GetText(strs.quest_log));
+
+            var allDone = quests.All(x => x.UserQuest.IsCompleted);
+
+            var tmrw = now.AddDays(1).Date;
+            var desc = GetText(strs.dailies_reset(TimestampTag.FromDateTime(tmrw, TimestampTagStyles.Relative)));
+            if (allDone)
+                desc = GetText(strs.dailies_done) + "\n" + desc;
+
+            embed.WithDescription(desc);
+
+            foreach (var res in quests)
+            {
+                if (res.Quest is null)
+                    continue;
+
+                embed.AddField(
+                    (res.UserQuest.IsCompleted ? IQuest.COMPLETED : IQuest.INCOMPLETE) + " " + res.Quest.Name,
+                    $"{res.Quest.Desc}\n\n" +
+                    res.Quest.ToString(res.UserQuest.Progress),
+                    true);
+            }
+
+            await Response().Embed(embed).SendAsync();
         }
-
-        await Response().Embed(embed).SendAsync();
     }
 }
